e756255eb73d8b85ff7cbb51d62a6827020ee037
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-29 19:04:40 -0800

    Finish the function

--- working.py
+++ working.py
@@ -47,16 +47,20 @@ def solve(bo):
    
    def print_board(bo):
    
+       for i in range(len(bo)):
+           if i % 3 and i != 0:
+               print("------------")
+               
+               for j in range(len(bo[0])):
+                   if j % 3 == 0 and j != 0:
+                      print(" | ", end="")
+     
+                      if j == 8:
+                         print(bo[i] [j])
+                      else:
+                         print(str(bo[i][j]) + " ")
    
-   
-   
-   
-   
-   
-    
-   
-   
-   
+   print_board(board)
    
    
    
