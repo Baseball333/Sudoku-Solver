385872871f3339b0697ff98183ac342ce3cf58d2
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-29 18:57:52 -0800

    Establish the print function
    Print the board recursively after mathematical execution.

--- working.py
+++ working.py
@@ -39,6 +39,13 @@ def solve(bo):
       box_x = pos[1] // 3
       box_y = pos[0] // 3
       
+      for i in range(box_y * 3, box_y*3 + 3):
+          for j in range(box_x * 3, box_y * 3 + 3):
+               if bo[i][j] == num and (i, j) != pos:
+                   return False
+          return True
+   
+   def print_board(bo):
    
    
    
@@ -46,9 +53,7 @@ def solve(bo):
    
    
    
-   
-   
-   
+    
    
    
    
