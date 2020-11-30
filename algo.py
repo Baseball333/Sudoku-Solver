b060712d55e56ea6b2d4ccfcaacad220a60b10f6
Baseball333 <52107974+Baseball333@users.noreply.github.com>
2020-11-29 19:17:15 -0800

    Write the main backtracking algorithm

--- algo.py
+++ algo.py
@@ -1,3 +1,14 @@
 # This file contains the backtracking algorithm
 # Also specifies an alternative instance of recursion
 
+# Main Algorithm
+def solve(bo):
+  
+    find = find_empty(bo)
+    if not find:
+       return True
+    else:
+       row, col = find
+   
+  
+  
